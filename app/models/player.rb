4c11b07d3ca25aaf2e50d7de78c622cbc26429b1
Egor Konnov <egorknv@gmail.com>
2020-10-26 17:42:38 +0800

    auth backend

---
+++ app/models/player.rb
@@ -0,0 +1,5 @@
+class Player < ActiveRecord::Base
+  extend Devise::Models
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+end
