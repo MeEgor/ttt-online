5362020cffaca32f693c5e6b8d525aa331197204
Egor Konnov <egorknv@gmail.com>
2020-11-02 18:34:23 +0800

    configure cable for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
+  config.action_cable.url = 'ws://174.138.16.174/cable' # ws:// is non-secure, wss:// is secure
+  config.action_cable.allowed_request_origins = [ 'http://174.138.16.174' ]
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
